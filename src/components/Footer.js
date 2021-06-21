1fcbf4b5e62df593307092be66301037565f3ab0
ammagamma EDU <51951521+AGedu@users.noreply.github.com>
2021-06-22 00:17:42 +0200

    Update Footer.js
    corretto link github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer(){
 
   return(
     <div className="footer mt-5 d-flex align-items-center justify-content-center" style={{height: 60}}>
-      <h6>MATTIA-CAM is an educational tool - <a href="https://github.com/AGedu/mattiaCAM.git">Source Code</a> - powered by <a href="https://ammagamma.com">ammagamma</a></h6>
+      <h6>MATTIA-CAM is an educational tool - <a href="https://github.com/AGedu/mattiaCAM">Source Code</a> - powered by <a href="https://ammagamma.com">ammagamma</a></h6>
     </div>
   )
 }
